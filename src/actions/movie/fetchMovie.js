51df4a5c3096f76e2b25d290f94b2c4dd4688c24
sarten8 <sarten8@gmail.com>
2019-09-17 06:10:18 -0300

    movie detail & pagination 2

--- src/actions/movie/fetchMovie.js
+++ src/actions/movie/fetchMovie.js
@@ -8,7 +8,7 @@ const fetchMovie = id => async dispatch => {
       method: 'GET',
       headers: {
         'content-type': 'application/json;charset=utf-8',
-        Authorization: `Bearer ${process.env.TOKEN}`,
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MThlNmY2ZjZjYjNhMGVkNzFhZmQ5OGM0NDc2NzgyNyIsInN1YiI6IjViYjc2NTIzYzNhMzY4MTUwYzAyYjNmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1i9oGthZ1qDREimJC8_bfJRQH_aqtliQw8N28PtPdQ8`,
       },
       url: `https://api.themoviedb.org/3/movie/${id}`,
     }
